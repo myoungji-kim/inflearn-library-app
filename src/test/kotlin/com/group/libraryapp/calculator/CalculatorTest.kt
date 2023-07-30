8e208f5ca7a6859edf894e8ede34ff2e5b837166
myoungji-kim <ddingji.kim@gmail.com>
2023-07-30 23:33:54 +0900

    [#1] add minusTest()

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -3,6 +3,7 @@ package com.group.libraryapp.calculator
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.minusTest()
 }
 
 class CalculatorTest {
@@ -15,6 +16,19 @@ class CalculatorTest {
 
         // then
         if (calculator.number != 8) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        if (calculator.number != 2) {
             throw IllegalStateException()
         }
     }
